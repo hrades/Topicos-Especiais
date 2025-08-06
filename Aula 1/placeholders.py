186922f5c95c067840724a7f64374bc556b141b4
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-08-06 08:21:55 -0300

    first commit

---
+++ Aula 1/placeholders.py
@@ -0,0 +1,6 @@
+valor_a = int(input('Digite o primeiro número: '))
+valor_b = int(input('Digite o segundo número: '))
+# Diferentes maneiras de mostrar a mensagem
+print('O resultado da multiplicação desses números é: {0}'.format(valor_a*valor_b))
+print('O resultado da multiplicação desses números é: '+ str(valor_a*valor_b))
+print(f'O resultado da multiplicação desses números é: {valor_a*valor_b}')
