ee66f7404066b35a267da9eb87c7a9a5723a64cf
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-08-06 08:11:37 -0300

    Update test.py

--- Aula 1/test.py
+++ Aula 1/test.py
@@ -1,4 +1,4 @@
-print('Hello World')
+print('Hello World') # Escreve no terminal
 
 # Toda "variável" é um objeto em python
 
@@ -8,4 +8,7 @@ a = a.upper()
 print(a.lower())
 
 a = 1.0 * 2
-print(type(a))
+print(type(a)) # Mostra qual a classe do objeto
+# Operações importantes
+a = 7//2 # Divisão inteiro
+a = 7%2 # Divisão resto
