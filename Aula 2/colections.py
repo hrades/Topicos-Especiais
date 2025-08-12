cf7c4f5ce03031bbe1a4dcb6ffa99b3921f5bccd
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-08-12 07:30:14 -0300

    Added Aula 2
    Anotações sobre lista

---
+++ Aula 2/colections.py
@@ -0,0 +1,13 @@
+from math import pi
+
+# Listas
+empty_list = list()  #Define lista vazia
+empty_list = ['banana', pi, True, [1,2,3,4], 8]
+
+    # Maneiras de acessar os itens de uma lista em ordem
+for item in empty_list:
+  print(item)
+i = 0
+for i in range(0, len(empty_list)):
+    print(empty_list[i])
+
