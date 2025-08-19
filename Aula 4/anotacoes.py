6f773d068e39e4931cc01d5ff1c9a95f07cd1471
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-08-19 08:32:00 -0300

    Update anotacoes.py

--- Aula 4/anotacoes.py
+++ Aula 4/anotacoes.py
@@ -1,6 +1,7 @@
 #import calculadora_areas
 import math
 import modulos as mod
+from datetime import datetime as dt
 #calculadora_areas.retangulo()
 
 # Uma classe e a estrutura que vai definir um objeto
@@ -16,6 +17,9 @@ class Carro: #Declara uma nova classe chamada carro
         self._cor = cor
     def mostra_dados(self):
         return f'Marca: {self._marca}; Modelo: {self._modelo}; Ano: {self._ano}; Cor: {self._cor}'
+    
+    def idade_carro(self):
+        return dt.now().year - self._ano
 
 if __name__ == '__main__':
     #print(math.__name__)
@@ -25,4 +29,6 @@ if __name__ == '__main__':
     novo_carro = Carro('Ford','KA',2013,'Preto') # Declara novo objeto com a classe Carro
     outro_carro = Carro('Chevrolet', 'Onix', 2022)
     print(novo_carro.mostra_dados())
+    print(f'A idade desse carro é {novo_carro.idade_carro()} anos')
     print(outro_carro.mostra_dados())
+    print(f'A idade desse carro é {outro_carro.idade_carro()} anos')
