5190b5f6901d688f8fbf3073a02292eb3ac1f81d
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-08-19 08:17:54 -0300

    Update anotacoes.py
    Trabalhando com parâmetros

--- Aula 4/anotacoes.py
+++ Aula 4/anotacoes.py
@@ -7,22 +7,21 @@ import modulos as mod
 
 class Carro: #Declara uma nova classe chamada carro
     # Método construtor
-    def __init__(self): # Executado automaticamente ao criar um novo objeto
-        # self indica que o objeto está associado à classe (parâmetro)
-        self.marca = 'Ford'
-        self.modelo = 'Maverick'
-        self.ano = 1979
-        self.cor = 'Preto'
+    def __init__(self, marca, modelo, ano, cor='Default'): # Executado automaticamente ao criar um novo objeto
+        # O argumento self é uma palavra reservada do sistema e não pode ser trocada para sua funcionalidade
+        # self indica que o objeto está associado à classe (parâmetro > valores associados ao objeto)
+        self._marca = marca
+        self._modelo = modelo
+        self._ano = ano
+        self._cor = cor
 
 if __name__ == '__main__':
     #print(math.__name__)
     #print(__name__)
     #var = mod.retangulo(2,5)
     #print(var)
-    novo_carro = Carro() # Declara novo objeto com a classe Carro
-    print(novo_carro.modelo)
-    novo_carro.modelo = 'F150 Apocallipse'
-    print(novo_carro.modelo)
+    novo_carro = Carro('Ford','KA',2013,'Preto') # Declara novo objeto com a classe Carro
+    outro_carro = Carro('Chevrolet', 'Onix', 2022)
 
 
 
