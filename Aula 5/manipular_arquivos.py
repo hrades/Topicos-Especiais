d3d7ede0304acb996da6698ec032ad064a2ae1c7
hrades <helorades@hotmail.com>
2025-08-27 07:20:07 -0300

    ler_arquivo added

--- Aula 5/manipular_arquivos.py
+++ Aula 5/manipular_arquivos.py
@@ -6,6 +6,10 @@ class Anotacoes():
 
     def __doc__(self):
         return 'Esta é uma classe que trabalha a manipulação de arquivos de texto'
+    
+    def ler_arquivo(self):
+        with open() as arquivo:
+            pass
 
 if __name__=='__main__':
     pass
