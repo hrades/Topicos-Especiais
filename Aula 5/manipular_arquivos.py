3fcdaf144e1492ac4b755d22ea55ddfd0e1e0ff5
hrades <helorades@hotmail.com>
2025-08-27 08:28:38 -0300

    append de novas linhas

--- Aula 5/manipular_arquivos.py
+++ Aula 5/manipular_arquivos.py
@@ -1,8 +1,8 @@
 # Manipular arquivos com python
 
 class Anotacoes:
-    def __init__(self):
-        pass
+    def __init__(self, endereco):
+        self._endereco = endereco
 
     def __doc__(self):
         return 'Esta é uma classe que trabalha a manipulação de arquivos de texto (txt, csv...)'
@@ -19,11 +19,16 @@ class Anotacoes:
             arquivo.write('Teste de escrita em arquivo')
 
     def ler_arquivo(self):
-        with open(r"C:\Users\061210035\Downloads\dados.csv", mode='r', encoding='UTF-8') as dados:
+        with open(self._endereco, mode='r', encoding='UTF-8') as dados:
             for linha in dados:
                 print(linha.strip().split(','))
 
+    def adicionar_dados(self, texto):
+        with open(self._endereco, mode='a', encoding='UTF-8') as dados:
+            dados.write(f'\n{texto}')
+
 
 if __name__=='__main__':
-    meu_teste = Anotacoes()
+    meu_teste = Anotacoes(r"C:\Users\061210035\Downloads\dados.csv")
+    meu_teste.adicionar_dados('11,2025-08-25,Mariana Costa,Câmera Canon EOS Rebel T7,1,3200.00')
     meu_teste.ler_arquivo()
