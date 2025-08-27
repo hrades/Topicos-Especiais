d798b7fc790d65d7f9fd2dcc7b08444af135a02d
hrades <helorades@hotmail.com>
2025-08-27 07:27:57 -0300

    inserir texto em arquivo abertp

--- Aula 5/manipular_arquivos.py
+++ Aula 5/manipular_arquivos.py
@@ -8,8 +8,12 @@ class Anotacoes():
         return 'Esta é uma classe que trabalha a manipulação de arquivos de texto'
     
     def ler_arquivo(self):
-        with open() as arquivo:
-            pass
+        # arquivo = open()
+        # arquivo.close()
+        
+        # Maneira mais fácil de utilizar o open()
+        with open(r"C:\Users\061210035\Downloads\teste.txt") as arquivo: # Método builtin para acessar um arquivo do sistema
+            arquivo.write('Teste de escrita em arquivo')
 
 if __name__=='__main__':
     pass
