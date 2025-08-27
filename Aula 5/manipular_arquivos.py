9f1355d3555fdd8ca6634220ebbb276093d51d3f
hrades <helorades@hotmail.com>
2025-08-27 07:17:54 -0300

    __doc__ added

--- Aula 5/manipular_arquivos.py
+++ Aula 5/manipular_arquivos.py
@@ -1,2 +1,11 @@
 # Manipular arquivos com python
 
+class Anotacoes():
+    def __init__(self):
+        pass
+
+    def __doc__(self):
+        return 'Esta é uma classe que trabalha a manipulação de arquivos de texto'
+
+if __name__=='__main__':
+    pass
