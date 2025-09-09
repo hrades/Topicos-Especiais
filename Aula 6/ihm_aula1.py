350cff11c3ead22be451a5abf577b0cc578edd0c
hrades <helorades@hotmail.com>
2025-09-09 08:09:54 -0300

    função de trocar cor no botão

--- Aula 6/ihm_aula1.py
+++ Aula 6/ihm_aula1.py
@@ -3,18 +3,28 @@ import tkinter
 # Classe responsável por criar e montar as ferramentas da GUI
 class Aplicativo:
     def __init__(self, parent): #parent é o objeto do tipo canvas
-        back_color = "#EBEBEB"
-        font_color = '#87CEFA'
-        parent.config(background=back_color)
+        self.back_color = "#EBEBEB"
+        self.font_color = '#87CEFA'
+        self.cor_botao = False
+        parent.config(background=self.back_color)
         # Ferramentas
-        lbl_texto =  tkinter.Label(parent, text= 'Teste',  #objeto tipo texto
+        self.lbl_texto =  tkinter.Label(parent, text= 'Teste',  #objeto tipo texto
                                    font=('Arial', 14, 'italic', 'underline'),
-                                   foreground=font_color,
-                                   background=back_color)
-        bot_teste = tkinter.Button(parent, text='Clique aqui')
+                                   foreground=self.font_color,
+                                   background=self.back_color)
+        self.bot_teste = tkinter.Button(parent, text='Clique aqui',
+                                        command=self.trocar_cor_texto)
         #Montagem
-        lbl_texto.pack() #mostrar na janela
-        bot_teste.pack()
+        self.lbl_texto.pack() #mostrar na janela
+        self.bot_teste.pack()
+
+    def trocar_cor_texto(self):
+        if self.cor_botao == False:
+            self.lbl_texto.config(foreground='red')
+            self.cor_botao = True
+        else:
+            self.lbl_texto.config(foreground=self.font_color)
+            self.cor_botao = False
 
 if __name__ == "__main__":
     window = tkinter.Tk() #Declara um objeto tipo canvas (janela)
