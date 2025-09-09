7f99c7701c432e2f63969085a0ee0164a8d12285
Heloisa Rades <helorades@hotmail.com>
2025-09-09 20:32:39 -0300

    updated caixa_moedas

--- Exercicios/caixa_moedas.py
+++ Exercicios/caixa_moedas.py
@@ -1,6 +1,6 @@
 troco = dict()
 notas = (100.0, 50.0, 20.0, 10.0, 5.0, 1.0, 0.5, 0.25, 0.1, 0.05, 0.01)
-valor_reais = ' '
+
 def calcular_troco(valor, reais):
     dinheiro = reais//valor
     if valor>1:
