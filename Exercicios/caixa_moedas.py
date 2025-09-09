0be679ce571f8c6b244b8bacf6293ed43d62a820
Heloisa Rades <helorades@hotmail.com>
2025-09-09 20:30:55 -0300

    calculadora completa

--- Exercicios/caixa_moedas.py
+++ Exercicios/caixa_moedas.py
@@ -1,4 +1,17 @@
-moedas = dict()
+troco = dict()
+notas = (100.0, 50.0, 20.0, 10.0, 5.0, 1.0, 0.5, 0.25, 0.1, 0.05, 0.01)
+valor_reais = ' '
+def calcular_troco(valor, reais):
+    dinheiro = reais//valor
+    if valor>1:
+        troco[f'{valor} reais'] = dinheiro
+    elif valor<1:
+        #centavos = 1/valor
+        troco[f'{valor*100} cents'] = dinheiro
+    elif valor==1:
+        troco['1 real'] = dinheiro
+    return reais%valor
+
 while True:
     valor_reais = input('Digite o valor a ser convertido (use . na vírgula): R$ ')
     try:
@@ -8,16 +21,12 @@ while True:
         continue
     
     # Moedas de 1
-    if valor_reais>0:
-        moedas_1 = valor_reais%1
-        moedas['1 real'] = moedas_1
-        valor_reais -= moedas_1
-        print(valor_reais)
-
-    '''else:
-        if valor_reais'''
-
-    print(moedas)
+    for real in notas:
+        if valor_reais>=real:
+            valor_reais = round(calcular_troco(real, valor_reais), 2)
+    
+        
     print(valor_reais)
+    print(troco)
     break
         
