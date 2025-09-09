694143ecbfa3bce855562a09a7d119c89a34a59d
hrades <helorades@hotmail.com>
2025-09-09 07:24:41 -0300

    configurando a tela

--- Aula 6/ihm_aula1.py
+++ Aula 6/ihm_aula1.py
@@ -6,5 +6,6 @@ class Aplicativo:
 
 if __name__ == "__main__":
     window = tkinter.Tk() #Declara um objeto tipo canvas (janela)
+    window.geometry('500x500') # X e Y
+    window.title('GUI teste') #Nome da tela
     window.mainloop() #Laço de repetição que mantém a tela ativa e funcional
-    
