e57a295db6d449f212d1decc6ed8bfdd27d4bed9
hrades <helorades@hotmail.com>
2025-09-10 07:40:10 -0300

    formatação resposta

--- Aula 6/ihm_troco.py
+++ Aula 6/ihm_troco.py
@@ -1,4 +1,5 @@
 from calculadora_troco import NotasMoedas
+from math import trunc
 import tkinter as tk
 
 class AplicativoTroco():
@@ -6,6 +7,10 @@ class AplicativoTroco():
         self.back_color = "#EBEBEB"
         self.font_color = '#87CEFA'
 
+        self.lbl_title = tk.Label(parent, text="Troque seu dinheiro aqui! \nDigite o valor",
+                                font=('Arial', 12),
+                                foreground='black',
+                                background=self.back_color)
         self.txtbox_valor = tk.Entry()
         self.bot_computar = tk.Button(parent, text='Calcular troco',
                                         command=self.resultado)
@@ -16,6 +21,7 @@ class AplicativoTroco():
                                 foreground='black',
                                 background=self.back_color)
 
+        self.lbl_title.pack()
         self.txtbox_valor.pack()
         self.bot_computar.pack()
         self.bot_apagar.pack()
@@ -34,14 +40,24 @@ class AplicativoTroco():
     def apagar_msg(self):
         self.lbl_msg.config(text=f"")
 
+    def formatar_troco(self, troco_dict):
+        
+        mensagem = "Troco:\n"
+        for nota, quantidade in troco_dict.items():
+            if quantidade > 0:
+                mensagem += f"{trunc(float(quantidade))} x {nota}\n"
+        return mensagem.strip()
+
     def resultado(self):
         valor = self.converter_texto()
-        moedas = NotasMoedas(valor)
-        self.lbl_msg.config(text=f'{moedas.mostrar_troco()}', foreground='black')
+        if valor is not None:
+            moedas = NotasMoedas(valor)
+            troco_formatado = self.formatar_troco(moedas.mostrar_troco())
+            self.lbl_msg.config(text=troco_formatado, foreground='black')
 
 if __name__=="__main__":
     window = tk.Tk() #Declara um objeto tipo canvas (janela)
-    window.geometry('200x200') # X e Y
+    window.geometry('200x500') # X e Y
     window.title('Calculadora de troco') #Nome da tela
     AplicativoTroco(window) #executa a classe Aplicativo com argumento como o canvas
     
