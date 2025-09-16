ec458bd397221e5e83976663a489a1ff54fddae9
hrades <helorades@hotmail.com>
2025-09-16 07:48:01 -0300

    added error treatment

--- Aula 7/aula_7.py
+++ Aula 7/aula_7.py
@@ -8,7 +8,7 @@ class Aplicativo:
         self.cmbbox_escolha = ttk.Combobox(parent, 
                                            values=('red', 'green', 'blue'))
         self.lbl_cor = ttk.Label(parent, text='Cor', font=('Arial', 14))
-        self.bot_cor = ttk.Button(parent, text='Mudar cor')
+        self.bot_cor = ttk.Button(parent, text='Mudar cor', command=self.change_color)
         
         self.show_app()
 
@@ -19,7 +19,10 @@ class Aplicativo:
         self.lbl_cor.pack(pady=15)
 
     def change_color(self):
-        pass
+        cor = self.cmbbox_escolha.get()
+        index_select = self.cmbbox_escolha.current() # Retorna o index do elemento selecionado no combo box. Retorna -1 se não está na lista
+        if index_select != -1:
+            self.lbl_cor.config(background=cor,foreground='white',text=index_select)
 
 if __name__=="__main__":
     window = tk.Tk()
