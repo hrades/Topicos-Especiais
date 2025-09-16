7df85c3f229451b64a536ef88f5b5f295fd817d8
hrades <helorades@hotmail.com>
2025-09-16 07:28:41 -0300

    iniciando o app

--- Aula 7/aula_7.py
+++ Aula 7/aula_7.py
@@ -2,10 +2,18 @@ import tkinter as tk
 from tkinter import ttk
 
 class Aplicativo:
-    def __init__(self):
-        pass
+    def __init__(self, parent: tk.Tk):
+        parent.geometry('300x300')
+        self.cmbbox_escolha = ttk.Combobox(parent, 
+                                           values=('red', 'green', 'blue'))
+        self.bot_cor = tk
+        
+        self.show_app()
+
+    def show_app(self):
+        self.cmbbox_escolha.pack()
 
 if __name__=="__main__":
     window = tk.Tk()
-
+    Aplicativo(window)
     window.mainloop()
