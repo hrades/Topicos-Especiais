c2a3eed19876e60027bc7c790f56a0da7073f304
hrades <helorades@hotmail.com>
2025-09-16 08:35:30 -0300

    Created calculadora_lei_de_Ohm.py

---
+++ Aula 7/calculadora_lei_de_Ohm.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import ttk
+
+class Aplicativo():
+    def  __init__(self, parent: tk.Tk):
+        parent.geometry('300x300')
+        self.lbl_lei = ttk.Label(parent, text='Calculadora de Lei de Ohm', font=('Calibri Light',14))
+        
+
+        self.show_features()
+
+    def show_features(self):
+        self.lbl_lei.pack()
+
+if __name__ == "__main__":
+    window = tk.Tk()
+    Aplicativo(window)
+    window.mainloop()
