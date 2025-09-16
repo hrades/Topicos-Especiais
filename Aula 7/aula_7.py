bb95324927ca5ed46eabd17ef2d8c0c8d676b8e5
hrades <helorades@hotmail.com>
2025-09-16 07:35:39 -0300

    Added features

--- Aula 7/aula_7.py
+++ Aula 7/aula_7.py
@@ -4,14 +4,19 @@ from tkinter import ttk
 class Aplicativo:
     def __init__(self, parent: tk.Tk):
         parent.geometry('300x300')
+        self.lbl_title = ttk.Label(parent, text='Escolha uma cor', font=('Arial', 16))
         self.cmbbox_escolha = ttk.Combobox(parent, 
                                            values=('red', 'green', 'blue'))
-        self.bot_cor = tk
+        self.lbl_cor = ttk.Label(parent, text='Cor', font=('Arial', 14))
+        self.bot_cor = ttk.Button(parent, text='Mudar cor')
         
         self.show_app()
 
     def show_app(self):
+        self.lbl_title.pack()
         self.cmbbox_escolha.pack()
+        self.bot_cor.pack(anchor='e')
+        self.lbl_cor.pack(pady=15)
 
 if __name__=="__main__":
     window = tk.Tk()
