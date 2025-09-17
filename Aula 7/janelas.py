391f1ca084925afe8b9705ad21f4a93c79797acc
hrades <helorades@hotmail.com>
2025-09-17 07:49:01 -0300

    building grid

--- Aula 7/janelas.py
+++ Aula 7/janelas.py
@@ -9,13 +9,19 @@ class Aplicativo:
         self.fr_quadro2 = ttk.Frame(parent, relief='groove') # Apenas a moldura - n necessariamente aparente
         self.lbl_texto2 = ttk.Label(self.fr_quadro2, text='Texto 2')
 
-        # self.show_pack() # Utilizando o pack para mostrar na tela
+        self.show_grid()
+        self.show_pack() # Utilizando o pack para mostrar na tela
 
+    # Utilizar somente um dos tipos para organizar a janela principal (parent)
+    # Podemos utilizar pack ou grid dentro dos contentores (frames)
     def show_pack(self):
-        self.fr_quadro1.pack()
         self.lbl_texto1.pack()
-        self.fr_quadro2.pack(ipadx=3, ipady=3) # Adicionando margens
+        #self.fr_quadro2.pack(ipadx=3, ipady=3) # Adicionando margens
         self.lbl_texto2.pack()
+
+    def show_grid(self):
+        self.fr_quadro1.grid(row=0, column=0)
+        self.fr_quadro2.grid(row=0, column=1)
 
 if __name__ == "__main__":
     janela = tk.Tk()
