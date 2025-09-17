d50ba7bf71fc4b83cd59d9cbebd92309d6e674e3
hrades <helorades@hotmail.com>
2025-09-17 07:24:10 -0300

    added examples

--- Aula 7/janelas.py
+++ Aula 7/janelas.py
@@ -3,11 +3,15 @@ from tkinter import ttk
 
 class Aplicativo:
     def __init__(self, parent):
-        self.fr_quadro1 = ttk.LabelFrame(parent, text='Quadro 1')
+        self.fr_quadro1 = ttk.LabelFrame(parent, text='Quadro 1') # Texto + linha da moldura
         self.lbl_texto1 = ttk.Label(self.fr_quadro1, text='Texto 1')
+        self.fr_quadro2 = ttk.Frame(parent, relief='groove') # Apenas a moldura - n necessariamente aparente
+        self.lbl_texto2 = ttk.Label(self.fr_quadro2, text='Texto 2')
 
         self.fr_quadro1.pack()
         self.lbl_texto1.pack()
+        self.fr_quadro2.pack()
+        self.lbl_texto2.pack()
 
 if __name__ == "__main__":
     janela = tk.Tk()
