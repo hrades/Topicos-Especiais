39a162374c96e477ca56983603b726d09ced14a0
hrades <helorades@hotmail.com>
2025-09-23 08:07:01 -0300

    elementos do app adicionados

--- Aula 7/calculadora_lei_de_Ohm.py
+++ Aula 7/calculadora_lei_de_Ohm.py
@@ -5,22 +5,53 @@ from leis_de_ohm import LeiDeOhm
 class Aplicativo():
     def  __init__(self, parent: tk.Tk):
         parent.geometry('300x300')
-        self.lbl_lei = ttk.Label(parent, text='Calculadora de Lei de Ohm', font=('Calibri Light',14))
-        self.fr_quadro1 = ttk.Frame(parent, relief='groove')
-        self.str_opt = tk.StringVar()
-        self.rdb_option1 = ttk.Radiobutton(self.fr_quadro1, text='1ª Lei de Ohm',
-                                           variable=self.str_opt,
-                                           value='1 Lei')
-        self.rdb_option2 = ttk.Radiobutton(self.fr_quadro1, text='2ª Lei de Ohm',
-                                           variable=self.str_opt,
-                                           value='2 Lei')
-        
-        self.show_features()
+        self.features(parent)    
+        self.show_pack()
+        self.show_grid()
 
-    def show_features(self):
+    def features(self, parent):
+        self.lbl_lei = ttk.Label(parent, text='Calculadora 1ª Lei de Ohm', font=('Calibri Light',14))
+        self.fr_quadro1 = ttk.LabelFrame(parent, text='Escolha para calcular',relief='groove')
+        self.str_opt = tk.StringVar()
+        self.rdb_tensao = ttk.Radiobutton(self.fr_quadro1, text='Tensão',
+                                           variable=self.str_opt,
+                                           value='V')
+        self.rdb_corrente = ttk.Radiobutton(self.fr_quadro1, text='Corrente',
+                                           variable=self.str_opt,
+                                           value='A')
+        self.rdb_resistencia = ttk.Radiobutton(self.fr_quadro1, text='Resistência',
+                                           variable=self.str_opt,
+                                           value='R')
+        self.fr_quadro2 = ttk.Frame(parent,relief='groove')
+        self.lbl_valor1 = ttk.Label(self.fr_quadro2, text='Valor 1: ')
+        self.txb_valor1 = ttk.Entry(self.fr_quadro2)
+        self.lbl_valor2 = ttk.Label(self.fr_quadro2, text='Valor 2: ')
+        self.txb_valor2 = ttk.Entry(self.fr_quadro2)
+        self.bot_calcular = ttk.Button(parent, text='Calcular', command=self.calcular)
+        self.fr_quadro3 = ttk.Frame(parent,relief='groove')
+        self.lbl_resultado = ttk.Label(self.fr_quadro3, text='Resultado: ')
+
+    def show_pack(self):
         self.lbl_lei.pack()
-        self.fr_quadro1.pack(anchor='w')
-        self.rdb_option1.pack()
+        self.fr_quadro1.pack(anchor='w', pady=5)
+        self.fr_quadro2.pack(anchor='w', pady=5)
+        self.bot_calcular.pack()
+        self.fr_quadro3.pack(anchor='w',pady=5)
+        
+    def show_grid(self):
+        self.rdb_tensao.grid(row=0, column=0)
+        self.rdb_corrente.grid(row=0, column=1)
+        self.rdb_resistencia.grid(row=0, column=2)
+        self.lbl_valor1.grid(row=0,column=0)
+        self.txb_valor1.grid(row=0,column=1)
+        self.lbl_valor2.grid(row=1,column=0)
+        self.txb_valor2.grid(row=1,column=1)
+        self.lbl_resultado.grid(row=0,column=0)
+
+    def calcular(self):
+        pass
+
+    #def 
 
 if __name__ == "__main__":
     window = tk.Tk()
