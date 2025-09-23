c3c16c077d129f446f96313119502e676703d205
hrades <helorades@hotmail.com>
2025-09-23 08:16:28 -0300

    tela arrumada

--- Aula 7/calculadora_lei_de_Ohm.py
+++ Aula 7/calculadora_lei_de_Ohm.py
@@ -5,6 +5,8 @@ from leis_de_ohm import LeiDeOhm
 class Aplicativo():
     def  __init__(self, parent: tk.Tk):
         parent.geometry('300x300')
+        parent.columnconfigure((0,1), weight=1)
+        parent.rowconfigure((0,1,2,3), weight=1)
         self.features(parent)    
         self.show_pack()
         self.show_grid()
@@ -27,15 +29,15 @@ class Aplicativo():
         self.txb_valor1 = ttk.Entry(self.fr_quadro2)
         self.lbl_valor2 = ttk.Label(self.fr_quadro2, text='Valor 2: ')
         self.txb_valor2 = ttk.Entry(self.fr_quadro2)
-        self.bot_calcular = ttk.Button(parent, text='Calcular', command=self.calcular)
         self.fr_quadro3 = ttk.Frame(parent,relief='groove')
+        self.bot_calcular = ttk.Button(self.fr_quadro3, text='Calcular', command=self.calcular)
         self.lbl_resultado = ttk.Label(self.fr_quadro3, text='Resultado: ')
+        self.bot_fechar = ttk.Button(self.fr_quadro3, text='Fechar aplicação',command=self.close_app)
 
     def show_pack(self):
-        self.lbl_lei.pack()
+        self.lbl_lei.pack(anchor='w')
         self.fr_quadro1.pack(anchor='w', pady=5)
         self.fr_quadro2.pack(anchor='w', pady=5)
-        self.bot_calcular.pack()
         self.fr_quadro3.pack(anchor='w',pady=5)
         
     def show_grid(self):
@@ -46,12 +48,15 @@ class Aplicativo():
         self.txb_valor1.grid(row=0,column=1)
         self.lbl_valor2.grid(row=1,column=0)
         self.txb_valor2.grid(row=1,column=1)
-        self.lbl_resultado.grid(row=0,column=0)
+        self.bot_calcular.grid(row=0,column=0,columnspan=2, pady=5)
+        self.lbl_resultado.grid(row=1,column=0, pady=5)
+        self.bot_fechar.grid(row=2,column=1,pady=5)
 
     def calcular(self):
         pass
 
-    #def 
+    def close_app(self):
+        pass
 
 if __name__ == "__main__":
     window = tk.Tk()
