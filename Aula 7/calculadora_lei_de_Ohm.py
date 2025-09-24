e269b65917f66565dbd6dd3f954f1c49b178de6f
hrades <helorades@hotmail.com>
2025-09-24 06:45:02 -0300

    posicionamento arrumado

--- Aula 7/calculadora_lei_de_Ohm.py
+++ Aula 7/calculadora_lei_de_Ohm.py
@@ -4,7 +4,7 @@ from leis_de_ohm import LeiDeOhm
 
 class Aplicativo():
     def  __init__(self, parent: tk.Tk):
-        parent.geometry('300x300')
+        parent.geometry('215x245')
         parent.columnconfigure((0,1), weight=1)
         parent.rowconfigure((0,1,2,3), weight=1)
         self.features(parent)    
@@ -27,21 +27,23 @@ class Aplicativo():
                                            variable=self.str_opt,
                                            value='R',
                                            command=self.update_label)
-        self.fr_quadro2 = ttk.Frame(parent,relief='groove')
-        self.lbl_valor1 = ttk.Label(self.fr_quadro2, text='Valor 1: ')
+        self.fr_quadro2 = ttk.Frame(parent,relief='flat')
+        self.lbl_valor1 = ttk.Label(self.fr_quadro2, text='            Valor 1: ')
         self.txb_valor1 = ttk.Entry(self.fr_quadro2)
-        self.lbl_valor2 = ttk.Label(self.fr_quadro2, text='Valor 2: ')
+        self.lbl_valor2 = ttk.Label(self.fr_quadro2, text='            Valor 2: ')
         self.txb_valor2 = ttk.Entry(self.fr_quadro2)
-        self.fr_quadro3 = ttk.Frame(parent,relief='groove')
-        self.bot_calcular = ttk.Button(self.fr_quadro3, text='Calcular', command=self.calcular)
+        self.fr_quadro3 = ttk.Frame(parent,relief='flat')
+        self.bot_calcular = ttk.Button(parent, text='Calcular', command=self.calcular)
         self.lbl_resultado = ttk.Label(self.fr_quadro3, text='Resultado: ')
-        self.bot_fechar = ttk.Button(self.fr_quadro3, text='Fechar aplicação',command=self.close_app)
+        self.bot_fechar = ttk.Button(parent, text='Fechar aplicação',command=self.close_app)
 
     def show_pack(self):
         self.lbl_lei.pack(anchor='w')
         self.fr_quadro1.pack(anchor='w', pady=5)
         self.fr_quadro2.pack(anchor='w', pady=5)
+        self.bot_calcular.pack(pady=5)
         self.fr_quadro3.pack(anchor='w',pady=5)
+        self.bot_fechar.pack(anchor='e', pady=5)
         
     def show_grid(self):
         self.rdb_tensao.grid(row=0, column=0)
@@ -51,9 +53,7 @@ class Aplicativo():
         self.txb_valor1.grid(row=0,column=1)
         self.lbl_valor2.grid(row=1,column=0)
         self.txb_valor2.grid(row=1,column=1)
-        self.bot_calcular.grid(row=0,column=0,columnspan=2, pady=5)
         self.lbl_resultado.grid(row=1,column=0, pady=5)
-        self.bot_fechar.grid(row=2,column=1,pady=5)
 
     def calcular(self):
         calculo = self.str_opt.get()
@@ -63,7 +63,8 @@ class Aplicativo():
             valor1 = float(valor1)
             valor2 = float(valor2)
         except:
-            messagebox.showerror('Erro!', 'Digite um número') #exemplo
+            if calculo != '':
+                messagebox.showerror('Erro de leitura!', 'Digite um número') #exemplo
 
         if calculo == 'V':
             resultado = LeiDeOhm.primeira_lei(corrente=valor1, resistencia=valor2)
@@ -75,7 +76,7 @@ class Aplicativo():
             resultado = 'select'
 
         if resultado == 'select':
-            messagebox.showwarning('Atenção!', 'Selecione uma das opções para calcular')
+            messagebox.showwarning('Seleção!', 'Selecione uma das opções para calcular')
             self.lbl_resultado.config(text=f'Resultado: ')
         else:
             self.lbl_resultado.config(text=f'Resultado: {str(resultado)}')
@@ -89,14 +90,14 @@ class Aplicativo():
         escolha = self.str_opt.get()
 
         if escolha == 'V':
-            self.lbl_valor1.config(text='Corrente (A):')
+            self.lbl_valor1.config(text='    Corrente (A):')
             self.lbl_valor2.config(text='Resistência (Ω):')
         elif escolha == 'A':
-            self.lbl_valor1.config(text='Tensão (V):')
+            self.lbl_valor1.config(text='      Tensão (V):')
             self.lbl_valor2.config(text='Resistência (Ω):')
         elif escolha == 'R':
-            self.lbl_valor1.config(text='Tensão (V):')
-            self.lbl_valor2.config(text='Corrente (A):')
+            self.lbl_valor1.config(text='      Tensão (V):')
+            self.lbl_valor2.config(text='    Corrente (A):')
 
 if __name__ == "__main__":
     window = tk.Tk()
