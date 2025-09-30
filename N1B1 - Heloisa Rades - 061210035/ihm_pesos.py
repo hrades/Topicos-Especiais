468d5bc874bffa3a778d534e96a5a5d76680f33b
Heloisa Rades <helorades@hotmail.com>
2025-09-30 15:56:24 -0300

    added features

--- N1B1 - Heloisa Rades - 061210035/ihm_pesos.py
+++ N1B1 - Heloisa Rades - 061210035/ihm_pesos.py
@@ -5,20 +5,38 @@ from calculadora_pesos import peso_linear
 class Aplicativo:
 
     def __init__(self, parent: tk.Tk):
-        parent.geometry('250x250')
+        parent.geometry('340x250')
         self.lbl_titulo = ttk.Label(parent, text='Calculadora de pesos')
         self.lbl_escolha1 = ttk.Label(parent, text='Escolha o perfil')
+        self.lbl_escolha2 = ttk.Label(parent, text='Escolha o material')
         self.str_perfil = tk.StringVar()
         self.rdb_redondo = ttk.Radiobutton(parent, text='Redondo',
                                            variable=self.str_perfil,
-                                           value='R')
+                                           value='R',
+                                           command=self.update_lbl)
         self.rdb_quadrado = ttk.Radiobutton(parent, text='Quadrado',
                                            variable=self.str_perfil,
-                                           value='Q')
+                                           value='Q',
+                                           command=self.update_lbl)
         self.rdb_triangular = ttk.Radiobutton(parent, text='Triangular',
                                            variable=self.str_perfil,
-                                           value='T')
-        
+                                           value='T',
+                                           command=self.update_lbl)
+        self.str_material = tk.StringVar()
+        self.rdb_aco = ttk.Radiobutton(parent, text='Aço',
+                                       variable=self.str_material,
+                                       value=1)
+        self.rdb_latao = ttk.Radiobutton(parent, text='Latao',
+                                       variable=self.str_material,
+                                       value=2)
+        self.rdb_aluminio = ttk.Radiobutton(parent, text='Alumínio',
+                                       variable=self.str_material,
+                                       value=3)
+        self.lbl_tamanho = ttk.Label(parent, text='Digite o valor (cm):')
+        self.txb_tamanho = ttk.Entry(parent)
+        self.bot_calcular = ttk.Button(parent, text='Calcular', command=self.calcular)
+        self.lbl_resultado = ttk.Label(parent, text='teste')
+
         self.show_grid()
         
     def show_grid(self):
@@ -26,9 +44,36 @@ class Aplicativo:
                              sticky='n', pady=5,
                              columnspan=3)
         self.lbl_escolha1.grid(row=1, column=0, sticky='w')
-        self.rdb_redondo.grid(row=2, column=0)
-        self.rdb_quadrado.grid(row=2, column=1)
-        self.rdb_triangular.grid(row=2, column=2)
+        self.rdb_redondo.grid(row=2, column=0,sticky='w')
+        self.rdb_quadrado.grid(row=2, column=1,sticky='w')
+        self.rdb_triangular.grid(row=2, column=2,sticky='w')
+        self.lbl_escolha2.grid(row=3, column=0, sticky='w', pady=2)
+        self.rdb_aco.grid(row=4, column=0,sticky='w')
+        self.rdb_latao.grid(row=4, column=1,sticky='w')
+        self.rdb_aluminio.grid(row=4, column=2,sticky='w')
+        self.lbl_tamanho.grid(row=5, column=1, pady=5)
+        self.txb_tamanho.grid(row=5, column=2, pady=5)
+        self.bot_calcular.grid(row=6, column=1)
+        self.lbl_resultado.grid(row=7,column=0, columnspan=3, pady=5)
+
+    def update_lbl(self):
+        perfil = self.str_perfil.get()
+        if perfil=='R':
+            self.lbl_tamanho.config(text='Digite o raio   (cm):')
+        elif perfil=='Q':
+            self.lbl_tamanho.config(text='Digite o lado  (cm):')
+        elif perfil=='T':
+            self.lbl_tamanho.config(text='Digite a base  (cm):')
+
+    def calcular(self):
+        perfil = self.str_perfil.get()
+        material = self.str_material.get()
+        tamanho = self.txb_tamanho.get()
+
+        try:
+            tamanho = float(tamanho)
+        except:
+            messagebox.showerror('Erro de leitura!', 'Digite números para realizar o cálculo\nUtilize . no lugar de ,')
 
 if __name__=="__main__":
     janela = tk.Tk()
