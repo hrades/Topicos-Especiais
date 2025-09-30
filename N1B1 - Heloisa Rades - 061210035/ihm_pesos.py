a7bf3c5af92d63a8fb82c0c6584fd5aff1b50e1e
Heloisa Rades <helorades@hotmail.com>
2025-09-30 15:01:50 -0300

    created ihm_pesos.py

---
+++ N1B1 - Heloisa Rades - 061210035/ihm_pesos.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+from calculadora_pesos import peso_linear
+
+class Aplicativo:
+
+    def __init__(self, parent: tk.Tk):
+        pass
+
+if __name__=="__main__":
+    janela = tk.Tk()
+    Aplicativo(janela)
+    janela.mainloop()
