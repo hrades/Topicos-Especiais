087707de05e517062d7014027205e5cebaef293a
hrades <helorades@hotmail.com>
2025-10-15 07:35:04 -0300

    created aplicativo_mongoDB.py

---
+++ Aula 9/aplicativo_mongoDB.py
@@ -0,0 +1,10 @@
+from inicio_MongoDB import Conexao_bd
+import tkinter as tk
+from tkinter import ttk
+
+class Aplicativo:
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    pass
