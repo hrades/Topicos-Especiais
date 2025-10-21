4400bb010ae7b7caecf2b26466e5df4482e8676f
hrades <helorades@hotmail.com>
2025-10-21 08:03:00 -0300

    added scrolledtext and function

--- Aula 10/interface_grafica.py
+++ Aula 10/interface_grafica.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
+from tkinter.scrolledtext import ScrolledText
 from pymongo import MongoClient
 
 class Conexao_bd:
@@ -16,6 +17,9 @@ class Aplicativo:
         self.frm_setores = ttk.LabelFrame(parent, text= "Gerenciamento de setores")
         self.txb_setor_id = ttk.Entry(self.frm_setores)
         self.txb_setor_nome = ttk.Entry(self.frm_setores)
+        self.scr_lista_setores = ScrolledText(self.frm_setores)
+        #Configurar tamanho da caixa de texto
+        self.scr_lista_setores.configure(width=10, height=10)
         
         self.frm_setores.pack(fill='both', expand=True)
         ttk.Label(self.frm_setores, text='Código do setor').grid(row=0,column=0)
@@ -23,7 +27,9 @@ class Aplicativo:
         self.txb_setor_id.grid(row=0, column=1)
         self.txb_setor_nome.grid(row=1, column=1)
         ttk.Button(self.frm_setores, text='Inserir', command=self.inserir_setor).grid(row=0,column=2,rowspan=2,sticky='ns')
+        self.scr_lista_setores.grid(row=2, column=0, columnspan=3, sticky='news')
 
+    # Inserir um novo setor no Banco de Dados
     def inserir_setor(self):
         # Atribui um novo objeto da classe de conexão com o banco de dados
         conexao = Conexao_bd()
@@ -36,7 +42,9 @@ class Aplicativo:
         except:
             messagebox.showerror('Erro', 'O registro nao pôde ser inserido.\nDigite um número que ainda não esteja na coleção')
         
-        
+    # Atualizar a caixa de texto com a lista de documentos da coleção setores
+    def listar_setores(self):
+        pass
 
 
 if __name__=="__main__":
